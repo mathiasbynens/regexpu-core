f680fb18d2fee63b7217335abd3768074276bd2e
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2018-01-15 21:28:45 +0100

    Update regjsparser

---
+++ demo.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const rewritePattern = require("./rewrite-pattern");
+const parse = require('regjsparser').parse;
+const generate = require('regjsgen').generate;
+const regenerate = require('regenerate');
+
+const pattern = String.raw`-`;
+
+console.log(generate(parse(pattern)));
+console.log(regenerate("-".codePointAt(0)).toString())
+
+const processedPattern = rewritePattern(pattern, 'u', { useUnicodeFlag: true });
+
+console.log(processedPattern);
+
+// throws
+new RegExp(processedPattern, 'u');
