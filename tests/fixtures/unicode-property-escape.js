26421924ce4b66f3d34be74b5944b937cdf3df7d
Huáng Jùnliàng <jlhwung@gmail.com>
2023-09-19 14:03:47 -0400

    extract fixtures to new modules

---
+++ tests/fixtures/unicode-property-escape.js
@@ -0,0 +1,497 @@
+const unicodePropertyEscapeFixtures = [
+	// https://unicode.org/reports/tr18/#RL1.2 item 1
+	{
+		'path': 'General_Category/Uppercase_Letter',
+		'expressions': [
+			'gc=Lu',
+			'gc=Uppercase_Letter',
+			'General_Category=Lu',
+			'General_Category=Uppercase_Letter',
+			'Lu',
+			'Uppercase_Letter'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL1.2 item 2a
+	{
+		'path': 'Script/Greek',
+		'expressions': [
+			'sc=Grek',
+			'sc=Greek',
+			'Script=Grek',
+			'Script=Greek'
+		]
+	},
+	{
+		'path': 'Script/Hiragana',
+		'expressions': [
+			'sc=Hira',
+			'sc=Hiragana',
+			'Script=Hira',
+			'Script=Hiragana'
+		]
+	},
+	{
+		'path': 'Script/Kawi',
+		'expressions': [
+			'sc=Kawi',
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL1.2 item 2b
+	{
+		'path': 'Script_Extensions/Greek',
+		'expressions': [
+			'scx=Grek',
+			'scx=Greek',
+			'Script_Extensions=Grek',
+			'Script_Extensions=Greek'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL1.2 item 3
+	{
+		'path': 'Binary_Property/Alphabetic',
+		'expressions': [
+			'Alpha',
+			'Alphabetic'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL1.2 item 4
+	{
+		'path': 'Binary_Property/Uppercase',
+		'expressions': [
+			'Upper',
+			'Uppercase'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL1.2 item 5
+	{
+		'path': 'Binary_Property/Lowercase',
+		'expressions': [
+			'Lower',
+			'Lowercase'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL1.2 item 6
+	{
+		'path': 'Binary_Property/White_Space',
+		'expressions': [
+			'WSpace',
+			'White_Space'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL1.2 item 7
+	{
+		'path': 'Binary_Property/Noncharacter_Code_Point',
+		'expressions': [
+			'NChar',
+			'Noncharacter_Code_Point'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL1.2 item 8
+	{
+		'path': 'Binary_Property/Default_Ignorable_Code_Point',
+		'expressions': [
+			'DI',
+			'Default_Ignorable_Code_Point'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL1.2 item 9a
+	{
+		'path': 'Binary_Property/Any',
+		'expressions': [
+			'Any'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL1.2 item 9b
+	{
+		'path': 'Binary_Property/ASCII',
+		'expressions': [
+			'ASCII'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL1.2 item 9c
+	{
+		'path': 'Binary_Property/Assigned',
+		'expressions': [
+			'Assigned'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/ASCII_Hex_Digit',
+		'expressions': [
+			'ASCII_Hex_Digit',
+			'AHex'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	// {
+	// 	'path': 'Bidi_Class/Arabic_Letter',
+	// 	'expressions': [
+	// 		'bc=AL',
+	// 		'bc=Arabic_Letter',
+	// 		'Bidi_Class=AL',
+	// 		'Bidi_Class=Arabic_Letter'
+	// 	]
+	// },
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Bidi_Control',
+		'expressions': [
+			'Bidi_C',
+			'Bidi_Control'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Bidi_Mirrored',
+		'expressions': [
+			'Bidi_M',
+			'Bidi_Mirrored'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Case_Ignorable',
+		'expressions': [
+			'CI',
+			'Case_Ignorable',
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Cased',
+		'expressions': [
+			'Cased'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Changes_When_NFKC_Casefolded',
+		'expressions': [
+			'CWKCF',
+			'Changes_When_NFKC_Casefolded'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Changes_When_Casefolded',
+		'expressions': [
+			'CWCF',
+			'Changes_When_Casefolded'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Changes_When_Casemapped',
+		'expressions': [
+			'CWCM',
+			'Changes_When_Casemapped'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Changes_When_Lowercased',
+		'expressions': [
+			'CWL',
+			'Changes_When_Lowercased'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Changes_When_Titlecased',
+		'expressions': [
+			'CWT',
+			'Changes_When_Titlecased'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Changes_When_Uppercased',
+		'expressions': [
+			'CWU',
+			'Changes_When_Uppercased'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Dash',
+		'expressions': [
+			'Dash'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Deprecated',
+		'expressions': [
+			'Dep',
+			'Deprecated'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Diacritic',
+		'expressions': [
+			'Dia',
+			'Diacritic'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Extender',
+		'expressions': [
+			'Ext',
+			'Extender'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Grapheme_Base',
+		'expressions': [
+			'Gr_Base',
+			'Grapheme_Base'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Grapheme_Extend',
+		'expressions': [
+			'Gr_Ext',
+			'Grapheme_Extend'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Hex_Digit',
+		'expressions': [
+			'Hex',
+			'Hex_Digit'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/ID_Continue',
+		'expressions': [
+			'IDC',
+			'ID_Continue'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/ID_Start',
+		'expressions': [
+			'IDS',
+			'ID_Start'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Ideographic',
+		'expressions': [
+			'Ideo',
+			'Ideographic'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/IDS_Binary_Operator',
+		'expressions': [
+			'IDSB',
+			'IDS_Binary_Operator'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/IDS_Trinary_Operator',
+		'expressions': [
+			'IDST',
+			'IDS_Trinary_Operator'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Join_Control',
+		'expressions': [
+			'Join_C',
+			'Join_Control'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Logical_Order_Exception',
+		'expressions': [
+			'LOE',
+			'Logical_Order_Exception'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Math',
+		'expressions': [
+			'Math'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Pattern_Syntax',
+		'expressions': [
+			'Pat_Syn',
+			'Pattern_Syntax'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Pattern_White_Space',
+		'expressions': [
+			'Pat_WS',
+			'Pattern_White_Space'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Quotation_Mark',
+		'expressions': [
+			'QMark',
+			'Quotation_Mark'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Radical',
+		'expressions': [
+			'Radical'
+		]
+	},
+	{
+		'path': 'Binary_Property/Regional_Indicator',
+		'expressions': [
+			'RI',
+			'Regional_Indicator'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Soft_Dotted',
+		'expressions': [
+			'SD',
+			'Soft_Dotted'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Sentence_Terminal',
+		'expressions': [
+			'STerm',
+			'Sentence_Terminal'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Terminal_Punctuation',
+		'expressions': [
+			'Term',
+			'Terminal_Punctuation'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Unified_Ideograph',
+		'expressions': [
+			'UIdeo',
+			'Unified_Ideograph'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/Variation_Selector',
+		'expressions': [
+			'VS',
+			'Variation_Selector'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/XID_Continue',
+		'expressions': [
+			'XIDC',
+			'XID_Continue'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	{
+		'path': 'Binary_Property/XID_Start',
+		'expressions': [
+			'XIDS',
+			'XID_Start'
+		]
+	},
+	// https://unicode.org/reports/tr18/#RL2.7
+	// {
+	// 	'path': 'Bidi_Paired_Bracket_Type/Open',
+	// 	'expressions': [
+	// 		'bpt=o',
+	// 		'bpt=Open',
+	// 		'Bidi_Paired_Bracket_Type=o',
+	// 		'Bidi_Paired_Bracket_Type=Open'
+	// 	]
+	// },
+	// https://unicode.org/reports/tr51/
+	{
+		'path': 'Binary_Property/Emoji',
+		'expressions': [
+			'Emoji'
+		]
+	},
+	// https://unicode.org/reports/tr51/
+	{
+		'path': 'Binary_Property/Emoji_Component',
+		'expressions': [
+			'Emoji_Component'
+		]
+	},
+	// https://unicode.org/reports/tr51/
+	{
+		'path': 'Binary_Property/Emoji_Modifier',
+		'expressions': [
+			'Emoji_Modifier'
+		]
+	},
+	// https://unicode.org/reports/tr51/
+	{
+		'path': 'Binary_Property/Emoji_Modifier_Base',
+		'expressions': [
+			'Emoji_Modifier_Base'
+		]
+	},
+	// https://unicode.org/reports/tr51/
+	{
+		'path': 'Binary_Property/Emoji_Presentation',
+		'expressions': [
+			'Emoji_Presentation'
+		]
+	},
+	// https://unicode.org/reports/tr51/proposed.html
+	{
+		'path': 'Binary_Property/Extended_Pictographic',
+		'expressions': [
+			'Extended_Pictographic'
+		]
+	},
+	{
+		'path': 'Script_Extensions/Yezidi',
+		'expressions': [
+			'scx=Yezi',
+			'scx=Yezidi',
+			'Script_Extensions=Yezi',
+			'Script_Extensions=Yezidi',
+		]
+	},
+	{
+		'path': 'Script_Extensions/Toto',
+		'expressions': [
+			'scx=Toto',
+			'Script_Extensions=Toto',
+		]
+	},
+];
+
+exports.unicodePropertyEscapeFixtures = unicodePropertyEscapeFixtures;
---
+++ tests/fixtures/unicode.js
@@ -0,0 +1,297 @@
+const FLAGS_WITH_UNICODE = 'u ui ug um uy uig uim uigm uigmy'.split(' ');
+const FLAGS_WITH_UNICODE_WITH_I = 'ui uig uim uigm uigmy'.split(' ');
+const FLAGS_WITH_UNICODE_WITHOUT_I = 'u ug um uy ugm ugmy'.split(' ');
+// Note: the leading space is important.
+const FLAGS_WITHOUT_UNICODE = ' i g m y ig im igm igmy'.split(' ');
+const FLAGS = FLAGS_WITH_UNICODE.concat(FLAGS_WITHOUT_UNICODE);
+
+const unicodeFixtures = [
+	{
+		'pattern': '.',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '.'
+	},
+	{
+		'pattern': '.',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '(?:[\\0-\\t\\x0B\\f\\x0E-\\u2027\\u202A-\\uD7FF\\uE000-\\uFFFF]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF]|[\\uD800-\\uDBFF](?![\\uDC00-\\uDFFF])|(?:[^\\uD800-\\uDBFF]|^)[\\uDC00-\\uDFFF])'
+	},
+	{
+		'pattern': '\\s',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '[\\t-\\r \\xA0\\u1680\\u2000-\\u200A\\u2028\\u2029\\u202F\\u205F\\u3000\\uFEFF]'
+	},
+	{
+		'pattern': '\\s',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '[\\t-\\r \\xA0\\u1680\\u2000-\\u200A\\u2028\\u2029\\u202F\\u205F\\u3000\\uFEFF]'
+	},
+	{
+		'pattern': '\\S',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '[\\0-\\x08\\x0E-\\x1F!-\\x9F\\xA1-\\u167F\\u1681-\\u1FFF\\u200B-\\u2027\\u202A-\\u202E\\u2030-\\u205E\\u2060-\\u2FFF\\u3001-\\uFEFE\\uFF00-\\uFFFF]'
+	},
+	{
+		'pattern': '\\S',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '(?:[\\0-\\x08\\x0E-\\x1F!-\\x9F\\xA1-\\u167F\\u1681-\\u1FFF\\u200B-\\u2027\\u202A-\\u202E\\u2030-\\u205E\\u2060-\\u2FFF\\u3001-\\uD7FF\\uE000-\\uFEFE\\uFF00-\\uFFFF]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF]|[\\uD800-\\uDBFF](?![\\uDC00-\\uDFFF])|(?:[^\\uD800-\\uDBFF]|^)[\\uDC00-\\uDFFF])'
+	},
+	{
+		'pattern': '[\\s\\S]',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '[\\0-\\uFFFF]'
+	},
+	{
+		'pattern': '[\\s\\S]',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '(?:[\\0-\\uD7FF\\uE000-\\uFFFF]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF]|[\\uD800-\\uDBFF](?![\\uDC00-\\uDFFF])|(?:[^\\uD800-\\uDBFF]|^)[\\uDC00-\\uDFFF])'
+	},
+	{
+		'pattern': '\\d',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '[0-9]'
+	},
+	{
+		'pattern': '\\D',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '[\\0-\\/:-\\uFFFF]'
+	},
+	{
+		'pattern': '\\D',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '(?:[\\0-\\/:-\\uD7FF\\uE000-\\uFFFF]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF]|[\\uD800-\\uDBFF](?![\\uDC00-\\uDFFF])|(?:[^\\uD800-\\uDBFF]|^)[\\uDC00-\\uDFFF])'
+	},
+	{
+		'pattern': '[\\d\\D]',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '[\\0-\\uFFFF]'
+	},
+	{
+		'pattern': '[\\d\\D]',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '(?:[\\0-\\uD7FF\\uE000-\\uFFFF]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF]|[\\uD800-\\uDBFF](?![\\uDC00-\\uDFFF])|(?:[^\\uD800-\\uDBFF]|^)[\\uDC00-\\uDFFF])'
+	},
+	{
+		'pattern': '\\w',
+		'flags': FLAGS_WITH_UNICODE_WITHOUT_I,
+		'transpiled': '[0-9A-Z_a-z]'
+	},
+	{
+		'pattern': '\\w',
+		'flags': FLAGS_WITH_UNICODE_WITH_I,
+		// Must match U+017F and U+212A.
+		'transpiled': '[0-9A-Z_a-z\\u017F\\u212A]'
+	},
+	{
+		'pattern': '\\W',
+		'flags': FLAGS_WITH_UNICODE_WITHOUT_I,
+		'transpiled': '(?:[\\0-\\/:-@\\[-\\^`\\{-\\uD7FF\\uE000-\\uFFFF]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF]|[\\uD800-\\uDBFF](?![\\uDC00-\\uDFFF])|(?:[^\\uD800-\\uDBFF]|^)[\\uDC00-\\uDFFF])'
+	},
+	{
+		'pattern': '\\W',
+		'flags': FLAGS_WITH_UNICODE_WITH_I,
+		// Must not match U+017F, U+212A, `K`, or `S` (unlike in ES6).
+		'transpiled': '(?:[\\0-\\/:-@\\[-\\^`\\{-\\u017E\\u0180-\\u2129\\u212B-\\uD7FF\\uE000-\\uFFFF]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF]|[\\uD800-\\uDBFF](?![\\uDC00-\\uDFFF])|(?:[^\\uD800-\\uDBFF]|^)[\\uDC00-\\uDFFF])'
+	},
+	{
+		'pattern': '[\\w\\W]',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '[\\0-\\uFFFF]'
+	},
+	{
+		'pattern': '[\\w\\W]',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '(?:[\\0-\\uD7FF\\uE000-\\uFFFF]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF]|[\\uD800-\\uDBFF](?![\\uDC00-\\uDFFF])|(?:[^\\uD800-\\uDBFF]|^)[\\uDC00-\\uDFFF])'
+	},
+	{
+		'pattern': '[\\uD834\\uDF06-\\uD834\\uDF08a-z]',
+		'flags': FLAGS_WITH_UNICODE_WITHOUT_I,
+		'transpiled': '(?:[a-z]|\\uD834[\\uDF06-\\uDF08])'
+	},
+	{
+		'pattern': '[\\uD834\\uDF06-\\uD834\\uDF08a-z]',
+		'flags': FLAGS_WITH_UNICODE_WITHOUT_I,
+		'transpiled': '(?:[a-z]|\\uD834[\\uDF06-\\uDF08])'
+	},
+	{
+		'pattern': '[\\u{1D306}-\\u{1D308}a-z]',
+		'flags': FLAGS_WITH_UNICODE_WITHOUT_I,
+		'transpiled': '(?:[a-z]|\\uD834[\\uDF06-\\uDF08])'
+	},
+	{
+		'pattern': '[\\u{0000000000001D306}-\\u{000000000000000000000001D308}a-z]',
+		'flags': FLAGS_WITH_UNICODE_WITHOUT_I,
+		'transpiled': '(?:[a-z]|\\uD834[\\uDF06-\\uDF08])'
+	},
+	{
+		'pattern': '[\\u{1D306}-\\u{1D308}a-z]+',
+		'flags': FLAGS_WITH_UNICODE_WITHOUT_I,
+		'transpiled': '(?:[a-z]|\\uD834[\\uDF06-\\uDF08])+'
+	},
+	{
+		// `s` and `k` case-fold to U+017F and U+212A.
+		'pattern': '[\\u{1D306}-\\u{1D308}a-z]',
+		'flags': FLAGS_WITH_UNICODE_WITH_I,
+		'transpiled': '(?:[a-z\\u017F\\u212A]|\\uD834[\\uDF06-\\uDF08])'
+	},
+	{
+		// `s` and `k` case-fold to U+017F and U+212A.
+		'pattern': '[\\u{1D306}-\\u{1D308}a-z]+',
+		'flags': FLAGS_WITH_UNICODE_WITH_I,
+		'transpiled': '(?:[a-z\\u017F\\u212A]|\\uD834[\\uDF06-\\uDF08])+'
+	},
+	{
+		// `s` and `k` case-fold to U+017F and U+212A.
+		'pattern': '[a-z]',
+		'flags': FLAGS_WITH_UNICODE_WITH_I,
+		'transpiled': '[a-z\\u017F\\u212A]'
+	},
+	{
+		// `s` and `k` case-fold to U+017F and U+212A.
+		'pattern': '[A-Z]',
+		'flags': FLAGS_WITH_UNICODE_WITH_I,
+		'transpiled': '[A-Z\\u017F\\u212A]'
+	},
+	{
+		'pattern': '[\\u017F\\u212A]',
+		'flags': FLAGS_WITH_UNICODE_WITHOUT_I,
+		'transpiled': '[\\u017F\\u212A]'
+	},
+	{
+		'pattern': '[\\u017F\\u212A]',
+		'flags': FLAGS_WITH_UNICODE_WITH_I,
+		'transpiled': '[KS\\u017F\\u212A]'
+	},
+	{
+		'pattern': '\\uD806\\uDCDF',
+		'flags': FLAGS_WITH_UNICODE_WITHOUT_I,
+		'transpiled': '(?:\\uD806\\uDCDF)'
+	},
+	{
+		// U+118DF case-folds to U+118BF.
+		'pattern': '\\uD806\\uDCDF',
+		'flags': FLAGS_WITH_UNICODE_WITH_I,
+		'transpiled': '(?:\\uD806[\\uDCBF\\uDCDF])'
+	},
+	{
+		'pattern': '[^a]',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '(?:(?![a\\uD800-\\uDFFF])[\\s\\S]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF])'
+	},
+	{
+		'pattern': '[^a]',
+		'flags': FLAGS_WITH_UNICODE_WITHOUT_I,
+		'transpiled': '(?:[\\0-`b-\\uD7FF\\uE000-\\uFFFF]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF]|[\\uD800-\\uDBFF](?![\\uDC00-\\uDFFF])|(?:[^\\uD800-\\uDBFF]|^)[\\uDC00-\\uDFFF])'
+	},
+	{
+		'pattern': '[^a]',
+		'flags': FLAGS_WITH_UNICODE_WITH_I,
+		'transpiled': '(?:(?![a\\uD800-\\uDFFF])[\\s\\S]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF])'
+	},
+	{
+		'pattern': '[ab]+',
+		'flags': FLAGS,
+		'transpiled': '[ab]+'
+	},
+	{
+		'pattern': '^(?:ab|cd)$',
+		'flags': FLAGS,
+		'transpiled': '^(?:ab|cd)$'
+	},
+	{
+		// Without the `u` flag, the character class contains two entries: one for
+		// each surrogate half.
+		'pattern': '[\\uD834\\uDF06]',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '[\\uD834\\uDF06]'
+	},
+	{
+		// With the `u` flag, the character class contains a single entry: one for
+		// each code point.
+		'pattern': '[\\uD834\\uDF06]',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '(?:\\uD834\\uDF06)'
+	},
+	{
+		'pattern': '\\uD834\\uDF06+',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '\\uD834\\uDF06+'
+	},
+	{
+		'pattern': '\\uD834\\uDF06+',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '(?:\\uD834\\uDF06)+'
+	},
+	{
+		'pattern': '\\uD834\\uDF06+',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '\\uD834\\uDF06+'
+	},
+	{
+		'pattern': '\\uD834\\uDF06+',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '(?:\\uD834\\uDF06)+'
+	},
+	{
+		// https://bugs.ecmascript.org/show_bug.cgi?id=3521#c3
+		'pattern': '\\u{D834}\\u{DF06}+',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '(?:\\uD834(?![\\uDC00-\\uDFFF]))(?:(?:[^\\uD800-\\uDBFF]|^)\\uDF06)+'
+	},
+	{
+		'pattern': '\\uD834\\uDF06{2,4}',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '\\uD834\\uDF06{2,4}'
+	},
+	{
+		'pattern': '\\uD834\\uDF06{2,4}',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '(?:\\uD834\\uDF06){2,4}'
+	},
+	{
+		'pattern': '\\uD834\\uDF06{2,4}',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '\\uD834\\uDF06{2,4}'
+	},
+	{
+		'pattern': '\\uD834\\uDF06{2,4}',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '(?:\\uD834\\uDF06){2,4}'
+	},
+	{
+		'pattern': '(a)\\1',
+		'flags': FLAGS_WITH_UNICODE_WITHOUT_I,
+		'transpiled': '(a)\\1'
+	},
+	{
+		// https://github.com/mathiasbynens/regexpu/issues/18
+		'pattern': '[]',
+		'flags': FLAGS,
+		'transpiled': '[]'
+	},
+	{
+		// https://github.com/mathiasbynens/regexpu/issues/19
+		'pattern': '(\\1)+\\1\\1',
+		'flags': FLAGS,
+		'transpiled': '(\\1)+\\1\\1'
+	},
+	// https://github.com/mathiasbynens/regexpu-core/issues/7
+	{
+		'pattern': '\\u03B8',
+		'flags': FLAGS_WITH_UNICODE_WITH_I,
+		'transpiled': '[\\u03B8\\u03F4]'
+	},
+	// https://github.com/mathiasbynens/regexpu-core/issues/11
+	{
+		'pattern': '\\/',
+		'flags': FLAGS_WITHOUT_UNICODE,
+		'transpiled': '\\/'
+	},
+	{
+		'pattern': '\\/',
+		'flags': FLAGS_WITH_UNICODE,
+		'transpiled': '\\/'
+	},
+];
+
+exports.unicodeFixtures = unicodeFixtures;
